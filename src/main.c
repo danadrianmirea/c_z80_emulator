288758a9ea060ab2adcaba95786f72c1662e8c06
Dan Adrian Mirea <damirea84@gmail.com>
2025-03-04 00:07:08 +0200

    actually remove the #undef main block completely

--- src/main.c
+++ src/main.c
@@ -273,10 +273,6 @@ void print_usage(const char *program_name) {
   printf("Example: %s 48.rom game.z80\n", program_name);
 }
 
-#ifdef _WIN32
-#undef main
-#endif
-
 int main(int argc, char *argv[]) {
   if (argc < 3) {
     print_usage(argv[0]);
